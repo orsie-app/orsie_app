9c7e845ad5898ac10437950db3c72c815d8c55e0
Suleman Mulla <mullasuleman12@gmail.com>
2020-02-16 06:10:58 -0500

    added service for mail (with and without html)

---
+++ services/send_mail.php
@@ -0,0 +1,21 @@
+<?php
+function sendMail($name, $email)
+{
+    $to = "$email";
+    $subject = "ORSIE Research Day Registration";
+    $from = "ORSIE <appliedresearch@durhamcollege.ca>";
+    $headers = "MIME-Version: 1.0" . "\n";
+    $headers .= "Content-type:text/html;charset=iso-8859-1" . "\n";
+    $headers .= "From: $from" . "\n";
+
+    $message = "Thank you for registering for ORSIE Research Day 2020.<br>- The ORSIE Team, Durham College";
+
+    // Sending email
+
+    // echo $message;
+    if (mail($to, $subject, $message, $headers)) {
+        return 'The mail has been sent successfully.';
+    } else {
+        return 'Unable to send email. Please try again.';
+    }
+}
