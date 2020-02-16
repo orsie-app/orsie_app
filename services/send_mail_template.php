c8726040862d7220031948b72b719f12679db23e
Suleman Mulla <mullasuleman12@gmail.com>
2020-02-16 18:06:55 -0500

    updated email-from name

--- services/send_mail_template.php
+++ services/send_mail_template.php
@@ -3,7 +3,7 @@ function sendMailTemplate($name, $email)
 {
     $to = "$email";
     $subject = "ORSIE Research Day Registration";
-    $from = "ORSIE <appliedresearch@durhamcollege.ca>";
+    $from = "Office of Research Services, Innovation and Entrepreneurship (ORSIE) <appliedresearch@durhamcollege.ca>";
     $headers = "MIME-Version: 1.0" . "\n";
     $headers .= "Content-type:text/html;charset=iso-8859-1" . "\n";
     $headers .= "From: $from" . "\n";
