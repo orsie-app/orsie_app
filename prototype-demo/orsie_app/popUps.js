3192c06a00fa129e78a1c78f3a4917741ffa4cda
Joshua Ryan <admin@joshua-ryan.ca>
2020-03-11 20:01:44 -0400

    update popup script to handle multiple pop ups

--- prototype-demo/orsie_app/popUps.js
+++ prototype-demo/orsie_app/popUps.js
@@ -1,24 +1,35 @@
+//function to clear the news ticker
+function clearPopUps(time){
+    setTimeout(() => {
+        document.querySelector('#newsTicker').innerText = ``;
+    }, time);
+};
+
 //add the pop up to the news ticker
 function displayPopUps(){
+    //variable to check if there are new pop ups
+    let newPopUps = false;
     //get the date
     let date = new Date();
     //the time in a 24hr string format
     let currentTime = `${date.getHours()}:${date.getMinutes()}`;
 
-    //check every pop up
-    for(popup of popUps){
+    //check each pop up
+    popUps.forEach(popup => {
         //if the pop up time matches the current time
         if(popup.time == currentTime){
+            newPopUps = true;
             //change the news ticker content to the new pop up 
-            document.querySelector('#newsTicker').innerText = `${popup.location}: ${popup.msg}`;
+            document.querySelector('#newsTicker').innerText += ` ${popup.location}: ${popup.msg}`;
         };
+    });
+
+    //if there are new pop ups added
+    if(newPopUps){
+        //clear the pop ups in 4 and a half minutes
+        clearPopUps(245000);
     };
 };
 
 //run the displayPopUps function every minute
 setInterval(displayPopUps, 60000);
-
-//function to clear the news ticker ... might not be necessary, but this is the code for it 
-function clearPopUps(){
-    document.querySelector('#newsTicker').innerText = ``;
-};
